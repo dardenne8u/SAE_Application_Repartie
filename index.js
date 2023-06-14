f0d663551403ae9073be56e40e7b9a331db837f8
miesch3u <nathanaelmiesch@gmail.com>
2023-06-14 16:42:48 +0200

    centrage sur Nancy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([48.691173, 6.184768], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
