27a634a32537f3cddc713ec8b67db8cc8f761dc8
drakoghir <gdardenne0@gmail.com>
2023-06-15 11:11:06 +0200

    getStationsInformation implements

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+import velibs from "./js/velibs.js";
+
 let map = L.map('map').setView([48.691173, 6.184768], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+
+const markers = [];
+
+const printStationsInformation = async () => {
+    let stations = await velibs.getStationsInformation();
+
+    stations.forEach(station => {
+        let marker = L.marker([station.lat, station.lon]).addTo(map);
+        marker.bindPopup(station.name);
+        markers.push({
+            id: station.station_id,
+            marker: marker
+        });
+    });
+
+}
+
+const printStationsStatus = async () => {
+
+}
+
+printStationsInformation()
