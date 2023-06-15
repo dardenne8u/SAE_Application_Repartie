1d1b82fcc4b2ac90cdf2c2011b63f620b14ea86f
Egario-akier <thierry.dissidia@gmail.com>
2023-06-15 15:45:47 +0200

    Évènements de circulation enregistrés

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import velibs from "./js/velibs.js";
+import circulation from "./js/circulation.js";
 
 let map = L.map('map').setView([48.691173, 6.184768], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,13 +10,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 const markers = [];
+const markersCircu = [];
 
 const printStationsInformation = async () => {
     let stations = await velibs.getStationsInformation();
 
     stations.forEach(station => {
         let marker = L.marker([station.lat, station.lon]).addTo(map);
-        marker.bindPopup(station.name);
+        marker.bindPopup(station.name, {color: 'orange'});
         markers.push({
             id: station.station_id,
             marker: marker
@@ -30,9 +32,26 @@ const printStationsStatus = async () => {
     stations.forEach(station => {
         let marker = markers.filter((marker) => marker.id === station.station_id)[0].marker;
         let popup = marker.getPopup();
-        popup.setContent(popup.getContent() + `<br>Vélo disponible : ${station.num_bikes_available}<br>Emplacement disponiblee : ${station.num_docks_available}`)
+        popup.setContent(popup.getContent() + `<br>Vélos disponibles : ${station.num_bikes_available}<br>Emplacements disponibles : ${station.num_docks_available}`)
+    });
+}
+
+const printCirculationEvents = async () => {
+    let circuData = await circulation.getCirculationData();
+    console.log(circuData);
+    circuData.forEach(incident => {
+        let marker = L.marker(incident.location.polyline.split(" ")).addTo(map);
+        let start = incident.starttime.split("T")[0];
+        let end = incident.endtime.split("T")[0];
+        marker.bindPopup(`${incident.location.street}<br>${incident.description}<br>Début incident : ${start}<br>Fin incident : ${end}`);
+        markersCircu.push({
+            id: incident.id,
+            marker: marker
+        });
     });
 }
 
 printStationsInformation()
     .then(()=>printStationsStatus());
+
+printCirculationEvents();
