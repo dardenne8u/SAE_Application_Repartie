9fc95a6adbf19d881a0da570594d5d05e0a34907
drakoghir <gdardenne0@gmail.com>
2023-06-15 15:52:10 +0200

    Creation du serveurhttp

--- java/src/ServiceCentral.java
+++ java/src/ServiceCentral.java
@@ -9,21 +9,31 @@ public class ServiceCentral implements HttpHandler {
 
 
     @Override
-    public void handle(HttpExchange t) throws IOException {
-        
+    public void handle(HttpExchange request) throws IOException {
+        /*
+         * C'est ici qu'on gere les requete http, faut regarder
+         * la documentation de HttpExchange
+         * Pour savoir, mais en gros je pense un switch devra faire l'affaire
+         * ne pas hésiter à déclarer des methodes privees
+         */
+
 
     }
 
     public static void main(String[] args) {
         HttpServer server = null;
         try {
+            // Creation de serveur (c'est pas dit dans la methode sur le port 8080)
             server = HttpServer.create(new InetSocketAddress(8080), 0);
         } catch (IOException e) {
             System.out.println("The port is already used");
             System.exit(1);
         }
 
+        // Ajoute un contexte donc en gros : http://localhost:8080/applications/myapp
         server.createContext("/applications/myapp", new ServiceCentral());
+
+        // Et ca on s'en fou
         server.setExecutor(null);
         server.start();
     }
